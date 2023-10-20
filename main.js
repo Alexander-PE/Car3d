5a5e208eb290338a594873ba466e3a91f2ced7d9
Alexander Perez <alexander.pereze02@gmail.com>
2023-10-20 12:39:26 -0400

    add lights and wheels geometry

--- main.js
+++ main.js
@@ -2,21 +2,28 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 100;
+camera.position.y = 50;
 
-const renderer = new THREE.WebGLRenderer();
+//setup renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
+// ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight); 
+
+
+//wheels
+function createWheels(){
+  const geometry = new THREE.BoxGeometry(12, 12, 33);
+  const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
+  const wheel = new THREE.Mesh( geometry, material );
+  return wheel
 }
-animate()
