13655a16716411f016613ba51bea91842b9a6985
Alexander Perez <alexander.pereze02@gmail.com>
2023-10-20 13:07:45 -0400

    testing shape for car build

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ const scene = new THREE.Scene();
 // camera
 const cameraWidth = 400 // size of camera
 const cameraHeight = cameraWidth / (window.innerWidth / window.innerHeight)
-const camera = new THREE.OrthographicCamera( cameraWidth / -2, cameraWidth / 2, cameraHeight / 2, cameraHeight / -2, 0, 1000);
+const camera = new THREE.OrthographicCamera(cameraWidth / -2, cameraWidth / 2, cameraHeight / 2, cameraHeight / -2, 0, 1000);
 camera.position.set(200, 200, 200); // angle of camera
 camera.lookAt(0, 5, 0);// orientation of camera (x,y,z)
 
 //setup renderer
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
@@ -24,18 +24,18 @@ scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 directionalLight.position.set(200, 500, 300);
-scene.add(directionalLight); 
+scene.add(directionalLight);
 
 //wheel
-function createWheels(){
+function createWheels() {
   // const geometry = new THREE.BoxGeometry(12, 12, 33);
   const geometry = new THREE.CylinderGeometry(7, 7, 33, 64);
   const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
-  const wheel = new THREE.Mesh( geometry, material );
+  const wheel = new THREE.Mesh(geometry, material);
   return wheel
 }
 
-function car(){
+function car() {
   const car = new THREE.Group();
 
   const back = createWheels();
@@ -47,8 +47,25 @@ function car(){
   const front = createWheels();
   front.position.y = 5;
   front.position.x = 25;
-  front.rotateX(Math.PI / 2); 
+  front.rotateX(Math.PI / 2);
   car.add(front);
+
+  const main = new THREE.Shape();
+  main.moveTo(-40, 8);
+  main.bezierCurveTo( 50, 2, 50, 11, 10, 30 );
+
+  const extrudeSettings = {
+    depth: 8,
+    bevelEnabled: true,
+    bevelSegments: 2,
+    steps: 2,
+    bevelSize: 1,
+    bevelThickness: 1
+  };
+  const geometry = new THREE.ExtrudeGeometry(main, extrudeSettings);
+  const mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial());
+
+  car.add(mesh);
 
   return car
 }
